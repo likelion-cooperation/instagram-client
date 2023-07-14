5bf8092f962b3e081c55e7f6a99d992e4ad9d476
LeeDongKyu <m3088787@naver.com>
2023-07-14 15:16:31 +0900

    [design] 마이페이지_헤더_설정 모달 디자인
    > 메뉴
    - 패딩 값 조정
    - 폰트 사이즈 변경
    - 텍스트 가운데 정렬
    - 호버 이펙트 커서 포인터 변경
    - 취소 클릭 시 모달 나가기
    
    >모달
    - border 조정
    - border-radius 조정

---
+++ profile/src/Header/SettingModal.js
@@ -0,0 +1,68 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import { styled } from 'styled-components';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  borderRadius: 2,
+  boxShadow: 24,
+  p: 0,
+};
+
+const StyledModalContainer = styled.div`
+`;
+
+const StyledModalItem = styled.div`
+  border-bottom : 1px solid gray;
+  text-align : center;
+  font-size : 20px;
+  font-weight:lighter;
+  padding : 12px;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export default function SettingModal() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <StyledModalContainer>
+
+        <Button onClick={handleOpen}>
+          <button className='settingButton'>
+            <svg xmlns="http://www.w3.org/2000/svg" widt h="30" height="30" fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16">
+              <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z" />
+              <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z" />
+            </svg>
+          </button></Button>
+        <Modal
+          open={open}
+          onClose={handleClose}
+        >
+          <Box sx={style}>
+            <div className='menuContainer'>
+              <StyledModalItem>앱 및 웹사이트</StyledModalItem>
+              <StyledModalItem>QR 코드</StyledModalItem>
+              <StyledModalItem>알림</StyledModalItem>
+              <StyledModalItem>설정 및 개인정보</StyledModalItem>
+              <StyledModalItem>관리 감독</StyledModalItem>
+              <StyledModalItem>로그아웃</StyledModalItem>
+              <StyledModalItem onClick={handleClose} >취소</StyledModalItem>
+            </div>
+          </Box>
+        </Modal>
+      </StyledModalContainer>
+    </div>
+  );
+}
