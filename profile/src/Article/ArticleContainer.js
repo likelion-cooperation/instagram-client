894716c6503e3baee0b499a3a82736279609b449
LeeDongKyu <m3088787@naver.com>
2023-07-16 16:43:46 +0900

    [feat] 게시물 호버 시 좋아요 수 댓글 수 노출 기능 추가

--- profile/src/Article/ArticleContainer.js
+++ profile/src/Article/ArticleContainer.js
@@ -4,22 +4,49 @@ import ArticleRow from './ArticleRow';
 import I1Src from './logo.jpg'
 
 const ArticleContainer = () => {
-    const ArticleSrc = [
-        { id: 0, src1: I1Src, src2: I1Src, src3: I1Src, alt1: '0-1', alt2:'0-2', alt3:'0-3'},
-        { id: 1, src1: I1Src, src2: I1Src, src3: I1Src, alt1: '1-1', alt2:'1-2', alt3:'1-3'},
-        { id: 2, src1: I1Src, src2: I1Src, src3: I1Src, alt1: '2-1', alt2:'2-2', alt3:'2-3'},
-
-    ];
+    const ArticleSrc =
+    {
+        row1: {
+            src1: I1Src, alt1: '0-1', fav1: '11', com1: '11',
+            src2: I1Src, alt2: '0-2', fav2: '12', com2: '12',
+            src3: I1Src, alt3: '0-3', fav3: '13', com3: '13',
+        },
+        row2: {
+            src1: I1Src, alt1: '1-1', fav1: '21', com1: '21',
+            src2: I1Src, alt2: '1-2', fav2: '22', com2: '22',
+            src3: I1Src, alt3: '1-3', fav3: '23', com3: '23',
+        },
+        row3: {
+            src1: I1Src, alt1: '2-1', fav1: '31', com1: '31',
+            src2: I1Src, alt2: '2-2', fav2: '32', com2: '32',
+            src3: I1Src, alt3: '2-3', fav3: '33', com3: '33',
+        },
+    };
 
 
     return (
         <>
             <div className='ArticleContainer container text-center'>
-            <hr />
+                <hr />
                 <div className='row '>
-                    <ArticleRow src1={ArticleSrc[0].src1} alt1={ArticleSrc[0].alt1} src2={ArticleSrc[0].src2} src3={ArticleSrc[0].src3}></ArticleRow>
-                    <ArticleRow src1={ArticleSrc[1].src1} alt1={ArticleSrc[1].alt1} src2={ArticleSrc[1].src2} src3={ArticleSrc[1].src3}></ArticleRow>
-                    <ArticleRow src1={ArticleSrc[2].src1} src2={ArticleSrc[2].src2} src3={ArticleSrc[2].src3}></ArticleRow>
+                    {/* 1행 */}
+                    <ArticleRow
+                        src1={ArticleSrc.row1.src1} fav1={ArticleSrc.row1.fav1} com1={ArticleSrc.row1.com1}
+                        src2={ArticleSrc.row1.src2} fav2={ArticleSrc.row1.fav2} com2={ArticleSrc.row1.com2}
+                        src3={ArticleSrc.row1.src3} fav3={ArticleSrc.row1.fav3} com3={ArticleSrc.row1.com3}>
+                    </ArticleRow>
+                    {/* 2행 */}
+                    <ArticleRow
+                        src1={ArticleSrc.row2.src1} fav1={ArticleSrc.row2.fav1} com1={ArticleSrc.row2.com1}
+                        src2={ArticleSrc.row2.src2} fav2={ArticleSrc.row2.fav2} com2={ArticleSrc.row2.com2}
+                        src3={ArticleSrc.row2.src3} fav3={ArticleSrc.row2.fav3} com3={ArticleSrc.row2.com3}>
+                    </ArticleRow>
+                    {/* 3행 */}
+                    <ArticleRow
+                        src1={ArticleSrc.row3.src1} fav1={ArticleSrc.row3.fav1} com1={ArticleSrc.row3.com1}
+                        src2={ArticleSrc.row3.src2} fav2={ArticleSrc.row3.fav2} com2={ArticleSrc.row3.com2}
+                        src3={ArticleSrc.row3.src3} fav3={ArticleSrc.row3.fav3} com3={ArticleSrc.row3.com3}>
+                    </ArticleRow>
                 </div>
             </div>
         </>
--- profile/src/Article/ArticleRow.js
+++ profile/src/Article/ArticleRow.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import { styled } from 'styled-components';
 import ModeCommentIcon from '@mui/icons-material/ModeComment';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 const style = {
     position: 'absolute',
@@ -30,43 +31,86 @@ const StyledModalRight = styled(StyledModalLeft)`
     background-color:blue;
   `;
 
-const ArticleDiv =styled.div`
+const ArticleDiv = styled.div`
+    position:relative;
     &:hover {
       cursor: pointer;
     }
 `;
 
+// const StyledModeCommentIcon = styled(ModeCommentIcon)`
+//     position:absolute;
+//     top: 50%;
+//     left:50%;
+// `;
+
+const FavorComConStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+};
+
+function ArticleFavoriteComment({fav, com}) {
+    return (
+        <div style={{
+            position: 'absolute',
+            top: '50%',
+            left: '30%',
+            color: 'white',
+        }}>
+            <FavoriteIcon className='me-2' style={{ color: 'white', }}></FavoriteIcon><span className='me-4'>{fav}</span>
+            <ModeCommentIcon className='me-2' style={{ color: 'white', }} /><span>{com}</span>
+        </div>
+    );
+};
 
 
-  
 
-const ArticleRow = ({ src1, src2, src3, alt1, alt2, alt3 }) => {
+
+
+const ArticleRow = ({ src1, src2, src3, alt1, alt2, alt3, fav1, fav2, fav3, com1, com2, com3}) => {
+
+
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
+    const [Ishover, setIsHover] = useState(0);
+    const onMouseEnter1 = () => setIsHover(1)
+    const onMouseEnter2 = () => setIsHover(2)
+    const onMouseEnter3 = () => setIsHover(3)
+    const onMouseLeave = () => setIsHover(0)
+
+
+
     return (
         <>
             <div className='w-100'></div>
-            <ArticleDiv onClick={handleOpen} className='ArticleImage col-3 m-1 p-0 '>
+            {/* Article1 */}
+            <ArticleDiv onMouseEnter={onMouseEnter1} onMouseLeave={onMouseLeave} onClick={handleOpen} className='ArticleImage col-3 m-1 p-0 '>
                 <img src={src1} alt={alt1} width="100%" height="100%"></img>
+                {Ishover===1 && <ArticleFavoriteComment fav={fav1} com={com1} style={FavorComConStyle}></ArticleFavoriteComment>}
             </ArticleDiv>
-            <ArticleDiv onClick={handleOpen} className='ArticleImage col-3 m-1 p-0'>
+            {/* Article2 */}
+            <ArticleDiv onMouseEnter={onMouseEnter2} onMouseLeave={onMouseLeave} onClick={handleOpen} className='ArticleImage col-3 m-1 p-0'>
                 <img src={src2} alt={alt2} width="100%" height="100%"></img>
+                {Ishover===2 && <ArticleFavoriteComment fav={fav2} com={com2} style={FavorComConStyle}></ArticleFavoriteComment>}
             </ArticleDiv>
-            <ArticleDiv onClick={handleOpen} className='ArticleImage col-3 m-1 p-0'>
+            {/* Article3 */}
+            <ArticleDiv onMouseEnter={onMouseEnter3} onMouseLeave={onMouseLeave} onClick={handleOpen} className='ArticleImage col-3 m-1 p-0'>
                 <img src={src3} alt={alt3} width="100%" height="100%"></img>
+                {Ishover===3 && <ArticleFavoriteComment fav={fav3} com={com3} style={FavorComConStyle}></ArticleFavoriteComment>}
             </ArticleDiv>
 
             {/* 모달 */}
             <Modal open={open} onClose={handleClose}>
                 <Box sx={style}>
-                        <StyledModalContainer className='container'>
-                            <div className='row'>
+                    <StyledModalContainer className='container'>
+                        <div className='row'>
                             <StyledModalLeft className='col'></StyledModalLeft>
                             <StyledModalRight className='col'></StyledModalRight>
-                            </div>
-                        </StyledModalContainer>
+                        </div>
+                    </StyledModalContainer>
                 </Box>
             </Modal>
             {/* End of 모달 */}
