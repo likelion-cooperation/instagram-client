50049d0316eeff5bf2fd1e12ea4f0b98d834d08d
LeeDongKyu <m3088787@naver.com>
2023-07-16 11:41:22 +0900

    [feat] 마이페이지 게시물 클릭 시 모달 기능 구현

--- profile/src/Article/ArticleContainer.js
+++ profile/src/Article/ArticleContainer.js
@@ -5,9 +5,9 @@ import I1Src from './logo.jpg'
 
 const ArticleContainer = () => {
     const ArticleSrc = [
-        { id: 0, src1: I1Src, src2: I1Src, src3: I1Src, alt1: 0-1, alt2:0-2, alt3:0-3},
-        { id: 1, src1: I1Src, src2: I1Src, src3: I1Src, alt1: 1-1, alt2:1-2, alt3:1-3},
-        { id: 2, src1: I1Src, src2: I1Src, src3: I1Src, alt1: 2-1, alt2:2-2, alt3:2-3},
+        { id: 0, src1: I1Src, src2: I1Src, src3: I1Src, alt1: '0-1', alt2:'0-2', alt3:'0-3'},
+        { id: 1, src1: I1Src, src2: I1Src, src3: I1Src, alt1: '1-1', alt2:'1-2', alt3:'1-3'},
+        { id: 2, src1: I1Src, src2: I1Src, src3: I1Src, alt1: '2-1', alt2:'2-2', alt3:'2-3'},
 
     ];
 
@@ -17,8 +17,8 @@ const ArticleContainer = () => {
             <div className='ArticleContainer container text-center'>
             <hr />
                 <div className='row '>
-                    <ArticleRow src1={ArticleSrc[0].src1} src2={ArticleSrc[0].src2} src3={ArticleSrc[0].src3}></ArticleRow>
-                    <ArticleRow src1={ArticleSrc[1].src1} src2={ArticleSrc[1].src2} src3={ArticleSrc[1].src3}></ArticleRow>
+                    <ArticleRow src1={ArticleSrc[0].src1} alt1={ArticleSrc[0].alt1} src2={ArticleSrc[0].src2} src3={ArticleSrc[0].src3}></ArticleRow>
+                    <ArticleRow src1={ArticleSrc[1].src1} alt1={ArticleSrc[1].alt1} src2={ArticleSrc[1].src2} src3={ArticleSrc[1].src3}></ArticleRow>
                     <ArticleRow src1={ArticleSrc[2].src1} src2={ArticleSrc[2].src2} src3={ArticleSrc[2].src3}></ArticleRow>
                 </div>
             </div>
--- profile/src/Article/ArticleRow.js
+++ profile/src/Article/ArticleRow.js
@@ -1,26 +1,75 @@
 import React from 'react';
-import './ArticleRow.css';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import { styled } from 'styled-components';
+import ModeCommentIcon from '@mui/icons-material/ModeComment';
 
-const ArticleRow = ({src1, src2, src3, alt1, alt2, alt3}) => {
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    borderRadius: 2,
+    boxShadow: 24,
+    p: 0,
+};
+
+const StyledModalContainer = styled.div`
+  `;
+
+const StyledModalLeft = styled.div`
+    width:100px;
+    height:100px;
+    border:1px solid black;
+    background-color:red;
+    `;
+
+const StyledModalRight = styled(StyledModalLeft)`
+    background-color:blue;
+  `;
+
+const ArticleDiv =styled.div`
+    &:hover {
+      cursor: pointer;
+    }
+`;
+
+
+
+  
+
+const ArticleRow = ({ src1, src2, src3, alt1, alt2, alt3 }) => {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     return (
         <>
-            {/* 빈줄 */}
             <div className='w-100'></div>
-            <div className='ArticleImage col-3 m-1 p-0 '>
-                <img src={src1} alt={alt1} width="100%" height="100%">
+            <ArticleDiv onClick={handleOpen} className='ArticleImage col-3 m-1 p-0 '>
+                <img src={src1} alt={alt1} width="100%" height="100%"></img>
+            </ArticleDiv>
+            <ArticleDiv onClick={handleOpen} className='ArticleImage col-3 m-1 p-0'>
+                <img src={src2} alt={alt2} width="100%" height="100%"></img>
+            </ArticleDiv>
+            <ArticleDiv onClick={handleOpen} className='ArticleImage col-3 m-1 p-0'>
+                <img src={src3} alt={alt3} width="100%" height="100%"></img>
+            </ArticleDiv>
 
-                </img>
-            </div>
-            <div className='ArticleImage col-3 m-1 p-0'>
-                <img src={src2} alt={alt2} width="100%" height="100%">
-
-                </img>
-            </div>
-            <div className='ArticleImage col-3 m-1 p-0'>
-                <img src={src3} alt={alt3} width="100%" height="100%">
-
-                </img>
-            </div>
+            {/* 모달 */}
+            <Modal open={open} onClose={handleClose}>
+                <Box sx={style}>
+                        <StyledModalContainer className='container'>
+                            <div className='row'>
+                            <StyledModalLeft className='col'></StyledModalLeft>
+                            <StyledModalRight className='col'></StyledModalRight>
+                            </div>
+                        </StyledModalContainer>
+                </Box>
+            </Modal>
+            {/* End of 모달 */}
         </>
     );
 };
