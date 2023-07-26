246ab2913f6f411ecc8aa26d487061f6742d464f
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2023-07-26 23:28:46 +0900

    [design] 회원가입 페이지 깨짐 현상으로 인한 디자인 리팩토링

--- profile/src/SignUp/SignUp.js
+++ profile/src/SignUp/SignUp.js
@@ -5,7 +5,6 @@ import TextIcon from '../Image/TextIcon.png'
 import FacebookIcon from '@mui/icons-material/Facebook';
 import GooglePlay from '../Image/GooglePlay.png'
 import Microsoft from '../Image/Microsoft.png'
-import axios from 'axios';
 
 
 const SytledPhone = styled.img`
@@ -47,23 +46,15 @@ const StyledTextIcon = styled.img`
     margin:5%;
 `;
 
-// const StyledButton1 = styled.button`
-//     width:100%;
-//     height:8%;
-//     background-color:#0095F6;
-//     color:white;
-//     font-size:15px;
-//     font-weight:bold;
-//     border:0px;
-//     border-radius:5px;
-//     `;
+const FacebookButton = styled.button`
+    background-color:#0095F6;
+    flex-direction:row;
+    text-align:center;
+`;
 
-// const StyledButton2 = styled(StyledButton1)`
-//     background-color:#4CB5F9;
-//     &:hover{
-//     background-color:#0095F6;
-// }
-// `;
+
+
+
 
 const StyledInput = styled.input`
 width: 100%;
@@ -72,6 +63,19 @@ border: 1px solid lightgray;
 background-color: #FAFAFA;
 margin-bottom: 5px;
 font-size: 12px;
+`;
+
+const StyledInputButton = styled(StyledInput)`
+    background-color: #4CB5F9;
+    color:white;
+    text-align:center;
+    margin:0px;
+    border:0px;
+    border-radius:5px;
+    height:30px;
+    font-weight:bold;
+    font-size:15px;
+    padding:0px;
 `;
 
 const DownloadContainer = styled.div`
@@ -88,8 +92,8 @@ height: 40px;
 function StyledHorizon({ text }) {
     return (
         <>
-            <div className='d-flex w-100 mt-2 mb-2'>
-                <div><hr className='ms-2 me-2' style={{ width: '120px' }} /></div>
+            <div className='d-flex w-100 mt-2 mb-2 ms-auto me-auto' style={{ justifyContent: 'center' }}>
+                <div><hr className='me-2' style={{ width: '120px' }} /></div>
                 {text}
                 <div><hr className='ms-2' style={{ width: '120px' }} /></div>
             </div>
@@ -110,7 +114,7 @@ const SignUp = () => {
                     <InputContainer className='mb-3'>
                         <StyledTextIcon src={TextIcon}></StyledTextIcon>
                         <div className='text-center fw-bold mb-2' style={{ fontSize: '15px', color: '#737373' }}>친구들의 사진과 동영상을 보려면<br /> 가입하세요.</div>
-                        {/* <StyledButton1><FacebookIcon /> faceBook으로 로그인</StyledButton1> */}
+                        <div className='d-flex justify-content-center'><FacebookButton className='d-flex justify-content-center'><div><FacebookIcon /></div><div>faceBook으로 로그인</div></FacebookButton></div>
                         <StyledHorizon text='또는'></StyledHorizon>
                         <form action='https://reqres.in/api/login' method='POST'>
                             <StyledInput placeholder='휴대폰 번호 또는 이메일 주소' name='loginId' />
@@ -118,15 +122,14 @@ const SignUp = () => {
                             <StyledInput placeholder='사용자 이름' name='nickname' />
                             <StyledInput placeholder='비밀번호' name='password' className='mb-3' />
                             <div className='mb-3' style={{ color: '#888888', fontSize: '5px' }}>저희 서비스를 이용하는 사람이 회원님의 연락처 정보를 Instagram에 업로드했을 수도 있습니다. <span style={{ color: '#22527F' }}>더 알아보기</span></div>
-                            <input type='submit' value='제출'></input>
-                            {/* <StyledButton2>가입하기</StyledButton2> */}
+                            <StyledInputButton type='submit' value='가입'></StyledInputButton>
                         </form>
                     </InputContainer>
                     <FooterContainer className='mb-3'>계정이 있으신가요? <HoverSpan>로그인</HoverSpan></FooterContainer>
                     <div className='mb-3' style={{ fontSize: '12px' }}>앱을 다운로드하세요.</div>
                     <DownloadContainer className='d-flex'><DownloadIcon className='me-2' src={GooglePlay} /><DownloadIcon src={Microsoft} /></DownloadContainer>
                 </div>
-            </StyledContainer>
+            </StyledContainer >
         </>
     );
 };
