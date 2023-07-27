e3448d04cbeff6f16caf34cad3012d02698c3ebf
LeeDongKyu <m3088787@naver.com>
2023-07-27 10:28:09 +0900

    [design] 마이페이지 헤더 디자인 수정
    - 작업 파일 병합 과정에서 레이아웃 깨짐 현상 발견, 이에 마이페이지 헤더 디자인을 수정함

--- profile/src/Profile/Header/Header.js
+++ profile/src/Profile/Header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import TestProfileImage from './TestProfileImage.jpg';
 import './Header.css';
-import SettingModal from './SettingModal';
+import SettingModal from './SettingModal'
 import { Link } from 'react-router-dom';
 
 
@@ -17,29 +17,35 @@ const Header = () => {
 
   return (
     <>
+    {/* 헤더 컨테이너 */}
       <div className="Header container justify-content-start text-start mb-5 mt-5">
+        {/* 1행 */}
         <div className="row align-items-center">
+          {/* 1행 1열(프로필 이미지) */}
           <div className="col-4">
             <div className="row justify-content-center">
               <img className="profileImage p-0" src={TestProfileImage} alt="ProfileImage" />
             </div>
           </div>
+          {/* 1행 2열 */}
           <div className="col-6 headInformationContainer">
+            {/* 닉네임, 프로필 편집, 설정 */}
             <div className="row justify-content-start">
-              <div className="col-3 fs-5" style={{ marginTop: '10px' }}>
+              {/* 닉네임 */}
+              <div className="col-3 fs-5 align-self-center" style={{ marginTop: '10px' }}>
                 {User.NickName}
               </div>
-              <div className="col-3" style={{ paddingLeft: '0px', marginTop: '10px' }}>
-                <Link to="/ProfileEdit">
-                  <button className="editButton fw-semibold px-4 py-1">
-                    프로필 편집
-                  </button>
+              {/* 프로필 편집 */}
+              <div className="col-3 align-self-center" style={{ paddingLeft: '0px', marginTop: '10px' }}>
+                <Link to="/ProfileEdit" > 
+                    <div className='editButton align-self-center fw-bold'>프로필 편집</div>
                 </Link>
               </div>
-              <div className="col-3 fs-5">
+              {/* 설정 버튼 */}
+              <div className="col-3 fs-5 align-self-center">
                 <SettingModal />
               </div>
-
+              {/* 게시물, 팔로워, 팔로우 수 */}
               <div className="w-100"></div>
               <div className="col-3">
                 게시물 <span className="fw-semibold">{User.ArticleCount}</span>
@@ -50,7 +56,7 @@ const Header = () => {
               <div className="col-3">
                 팔로우 <span className="fw-semibold">{User.FollowsCount}</span>
               </div>
-
+              {/* 이름, 소개글 */}
               <div className="w-100"></div>
               <div className="col">
                 <span className="fw-semibold">{User.Name}</span>
